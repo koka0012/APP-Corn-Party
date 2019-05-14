1e652bb45de84ae09bd764f23ee20198e985c0b0
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2019-05-14 11:40:03 -0400

    change about text

--- src/screens/Usuario/sobre.ts
+++ src/screens/Usuario/sobre.ts
@@ -1,11 +1,14 @@
 export default `
 Aplicativo desenvolvido pela turma de Análise e Desenvolvimento de Sistemas (2017/1) - Faculdade FASIPE. 
 
+Coordenador do Curso [Adriano Barreto](https://br.linkedin.com/in/adriano-barreto-5707664a)
+
+Um projeto coordenado pelo professor [Rodrigo J. Paiva](https://www.linkedin.com/in/rodrigojpaiva)
+
 Agradecimento em especial aos responsáveis pela conclusão do projeto; 
 - [Augusto Chmieleski](https://www.linkedin.com/in/augusto-silveira-917758143/) - Responsável pelo desenvolvimento Mobile.
 - [Gabriel Marcato](https://www.linkedin.com/in/gabriel-marcato-604651179/) - Responpável pelo desenvolvimento Front-end. 
 - [Mauro Moura](https://www.linkedin.com/in/mauro-moura-1291b616b/) - Responsável pelo desenvolvimento Back-end.
-- Rodrigo Paiva - Professor e orientador responsável pelo projeto.
 
 E aos demais alunos que colaboraram direta e inderetamente para o mesmo.
 `;
